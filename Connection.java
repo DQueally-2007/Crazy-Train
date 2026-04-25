af94fb639cd825f790fcd265b8d348894c5ec2e2
Daniel <d.queally@lancaster.ac.uk>
2026-04-25 12:06:58 +0100

    I've fleshed out the line, station and connection classes to create a descending series of arrays within objects containing objects which in turn contain other arrays of smaller objects. I'm not certain if this methodology will work when I finally come to apply my pathfinding algorithm but we shall see I suppose.

---
+++ Connection.java
@@ -0,0 +1,18 @@
+
+
+public class Connection
+{
+    private String lineColour;
+    private String fromStation;
+    private String toStation;
+    private int travelTime;
+
+    public Connections(String colour, String from, String to, int time)
+    {
+        lineColour = colour;
+        fromStation = from;
+        toStation = to;
+        travelTime = time;
+    }
+
+}
